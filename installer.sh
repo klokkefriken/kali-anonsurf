7a8f75ad716e190dfd5f1eb513c29b0a6da565ab
Your Name <you@example.com>
2021-02-07 05:55:21 +0100

    updated the script

--- installer.sh
+++ installer.sh
@@ -1,35 +1,78 @@
 #!/bin/bash
 
-# Ensure we are being ran as root
-if [ $(id -u) -ne 0 ]; then
-	echo "This script must be ran as root"
-	exit 1
+
+if [ $(id -u) -ne 0 ]; then 
+echo "This script must be ran as root"
+exit 1
 fi
 
-# For upgrades and sanity check, remove any existing i2p.list file
+&&
+
 rm -f /etc/apt/sources.list.d/i2p.list
 
-# Compile the i2p ppa
-echo "deb http://deb.i2p2.no/ unstable main" > /etc/apt/sources.list.d/i2p.list # Default config reads repos from sources.list.d
-wget https://geti2p.net/_static/i2p-debian-repo.key.asc -O /tmp/i2p-debian-repo.key.asc # Get the latest i2p repo pubkey
-apt-key add /tmp/i2p-debian-repo.key.asc # Import the key
-rm /tmp/i2p-debian-repo.key.asc # delete the temp key
-apt-get update # Update repos
+&&
+
+
+echo "deb http://deb.i2p2.no/ unstable main" > /etc/apt/sources.list.d/i2p.lis
+
+&&
+wget https://geti2p.net/_static/i2p-debian-repo.key.asc -O /tmp/i2p-debian-repo.key.asc
+
+&&
+apt-key add /tmp/i2p-debian-repo.key.asc 
+
+&&
+
+rm /tmp/i2p-debian-repo.key.asc
+
+&&
+
+apt-get update
+
+&&
 
 if [[ -n $(cat /etc/os-release |grep kali) ]]
 then
-	apt-get install libservlet3.0-java 
-	wget http://ftp.us.debian.org/debian/pool/main/j/jetty8/libjetty8-java_8.1.16-4_all.deb
-	dpkg -i libjetty8-java_8.1.16-4_all.deb # This should succeed without error
-	apt-get install libecj-java libgetopt-java libservlet3.0-java glassfish-javaee ttf-dejavu i2p i2p-router libjbigi-jni #installs i2p and other dependencies
-	apt-get -f install # resolves anything else in a broken state
+
+
+&&
+
+apt-get install libservlet3.0-java 
+
+&&
+
+wget http://ftp.us.debian.org/debian/pool/main/j/jetty8/libjetty8-java_8.1.16-4_all.deb
+
+&&
+
+dpkg -i libjetty8-java_8.1.16-4_all.deb
+
+&&
+
+apt-get install libecj-java libgetopt-java libservlet3.0-java glassfish-javaee ttf-dejavu i2p i2p-router libjbigi-jni 
+
+&&
+
+apt-get -f install
 fi
 
-apt-get install -y i2p-keyring #this will ensure you get updates to the repository's GPG key
-apt-get install -y secure-delete tor i2p # install dependencies, just in case
+&&
 
-# Configure and install the .deb
-dpkg-deb -b kali-anonsurf-deb-src/ kali-anonsurf.deb # Build the deb package
+apt-get install -y i2p-keyring
+
+&&
+
+apt-get install -y secure-delete tor i2pe
+
+
+dpkg-deb -b kali-anonsurf-deb-src/ kali-anonsurf.deb
+
+&&
+
 dpkg -i kali-anonsurf.deb || (apt-get -f install && dpkg -i kali-anonsurf.deb) # this will automatically install the required packages
+
+
+echo "Done! hack the planet!"
+
 
 exit 0
